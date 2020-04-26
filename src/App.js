386f7954c5b76b72b56119ea2085d8d8d84a9848
jaziel-luby <jaziel@n22.com.br>
2020-04-26 17:48:16 -0300

    221 - building the checkout container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import Layout from './hoc/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder/>
+        <Checkout/>
       </Layout>
     </div>
   );
