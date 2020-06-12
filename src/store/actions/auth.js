07fba5850af480da80c33f221c8e2392a20444e9
jaziel-luby <jaziel@n22.com.br>
2020-06-12 11:47:18 -0300

    327 - getting a token from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -20,8 +21,26 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, passoword) => {
+export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC9wAdn9UAhI7hTS6y9N0dMHHXnDYAgbq4 ",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail());
+      });
   };
 };
