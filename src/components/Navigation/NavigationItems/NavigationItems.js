21668baa93b9de758c75ac63e0cfa21d7f11e8ee
jaziel-luby <jaziel@n22.com.br>
2020-06-12 10:46:09 -0300

    325 - adding an auth form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './NavigationItems.css';
-import NavigationItem from './NavigationItem/NavigationItem';
+import "./NavigationItems.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = () => (
-    <ul className="NavigationItems">
-       <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-       <NavigationItem link="/orders">Orders</NavigationItem>
-    </ul>
+  <ul className="NavigationItems">
+    <NavigationItem link="/" exact>
+      Burger Builder
+    </NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
+    <NavigationItem link="/auth">Authenticate</NavigationItem>
+  </ul>
 );
 
-export default navigationItems
+export default navigationItems;
