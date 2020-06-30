cf9757948b67700d6241bc3280473a5a02793692
jazielg <jazielgj1995@gmail.com>
2020-06-29 21:36:14 -0300

    349 - removing console.log()s

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,36 @@
-import React from 'react'
+import React from "react";
 
-import Aux from '../../../hoc/Aux/Aux'
+import Aux from "../../../hoc/Aux/Aux";
 
-import Button from '../../UI/Button/Button'
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
-
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
-                        {props.ingredients[igKey]}
-                </li>
-            )
-        })
-
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
-}
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+        {props.ingredients[igKey]}
+      </li>
+    );
+  });
 
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
 
-export default orderSummary
+export default orderSummary;
