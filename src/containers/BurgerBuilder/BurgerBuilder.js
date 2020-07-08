a7aeb57ce407d90214e9e61289464c3211fa7c39
jazielg <jazielgj1995@gmail.com>
2020-07-07 21:10:38 -0300

    361 - testing containers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import withErrorhandler from "../../hoc/withErrorHandle/withErrorHandle";
 import axios from "../../axios-orders";
 import * as actions from "../../store/actions/index";
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false,
   };
