7899108ec52ada9d9c0e61808f21fa25b452a79e
jazielg <jazielgj1995@gmail.com>
2020-08-11 21:17:21 -0300

    415 - moving logic from the action creator to a saga

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import * as serviceWorker from "./serviceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
-import { logoutSaga } from "./store/sagas/auth";
+import { watchAuth } from "./store/sagas";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -36,7 +36,7 @@ const store = createStore(
   composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
 );
 
-sagaMiddleware.run(logoutSaga);
+sagaMiddleware.run(watchAuth);
 
 const app = (
   <Provider store={store}>
