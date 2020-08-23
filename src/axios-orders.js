2fc9007fcde0a8c8750fb4c168906f3c1ada8953
jazielg <jazielgj1995@gmail.com>
2020-08-23 12:55:43 -0300

    455 - converting witherrorhandler hoc

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://react-udemy-burger-ea744.firebaseio.com/'
+  baseURL: "https://react-udemy-burger-ea744.firebaseio.com/",
 });
 
 export default instance;
